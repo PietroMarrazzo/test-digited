9642f4a1fcfa7b1fbe1213727f5463aab48c0824
Pietro A. Marrazzo <marrazzo.pietroalfonso@gmail.com>
2025-07-24 12:19:25 +0200

    feat(MASTER): create project and add libraries

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Master } from "./components/Master";
+
+function App() {
+  return (
+    <>
+      {/* Master component is the handler of generic and high logics shared between other components */}
+      <Master />
+    </>
+  );
+}
+
+export default App;
